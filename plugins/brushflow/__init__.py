33f708217f9abad88ed76707527ab57bae1478a1
HelloKieth <luopeng63@gmail.com>
2023-11-23 18:55:54 +0800

    Update __init__.py

--- plugins/brushflow/__init__.py
+++ plugins/brushflow/__init__.py
@@ -33,9 +33,9 @@ class BrushFlow(_PluginBase):
     # 主题色
     plugin_color = "#FFD54E"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
-    plugin_author = "jxxghp"
+    plugin_author = "kieth"
     # 作者主页
     author_url = "https://github.com/jxxghp"
     # 插件配置项ID前缀
