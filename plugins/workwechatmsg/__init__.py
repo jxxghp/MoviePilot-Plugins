58759cde8b1d020a67cc0231adc2f977c4135189
叮叮当 <604054726@qq.com>
2023-11-27 18:18:26 +0800

    fix

--- plugins/workwechatmsg/__init__.py
+++ plugins/workwechatmsg/__init__.py
@@ -26,7 +26,7 @@ class WorkWechatMsg(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "workwechatmsg_"
     # 加载顺序
-    plugin_order = 27
+    plugin_order = 28
     # 可使用的用户级别
     auth_level = 1
 
