29e3833f6c11485b1d4cba4ff2624959342648a9
nelson <nelson.cheng.zhao@outlook.com>
2024-01-08 11:16:37 +0800

    修改版本号，添加测试代码

--- plugins/pushplusmsg/__init__.py
+++ plugins/pushplusmsg/__init__.py
@@ -16,7 +16,7 @@ class PushPlusMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Pushplus_A.png"
     # 插件版本
-    plugin_version = "1.0.1"
+    plugin_version = "0.1"
     # 插件作者
     plugin_author = "cheng"
     # 作者主页
