54531af8dd34edd20bd63dfda5766689f5cbb893
jxxghp <jxxghp@gmail.com>
2024-01-19 10:56:42 +0800

    更新 __init__.py

--- plugins/autosignin/__init__.py
+++ plugins/autosignin/__init__.py
@@ -36,7 +36,7 @@ class AutoSignIn(_PluginBase):
     # 插件图标
     plugin_icon = "signin.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
