290fbb3d82824a37653fbfddc894aa2c7dd8aa89
jxxghp <jxxghp@gmail.com>
2024-01-25 08:32:19 +0800

    更新 __init__.py

--- plugins/iyuuautoseed/__init__.py
+++ plugins/iyuuautoseed/__init__.py
@@ -34,7 +34,7 @@ class IYUUAutoSeed(_PluginBase):
     # 插件图标
     plugin_icon = "IYUU.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
