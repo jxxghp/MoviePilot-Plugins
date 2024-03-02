b0c0819aaa24ac57626bdf0c5abbebaa46f24ecd
luojianquan443 <2569646547@qq.com>
2024-03-03 02:06:39 +0800

    1.5

--- plugins/vcbanime/__init__.py
+++ plugins/vcbanime/__init__.py
@@ -60,7 +60,7 @@ class VCBAnime(_PluginBase):
     # 插件图标
     plugin_icon = "directory.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "luojianquan443"
     # 作者主页
