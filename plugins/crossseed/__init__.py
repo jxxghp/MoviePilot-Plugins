23ffbdcd82245fffeea09574724ac9e45cb33ba9
ljmeng <menglingjia0214@gmail.com>
2024-03-28 01:23:19 +0800

    升级版本

--- plugins/crossseed/__init__.py
+++ plugins/crossseed/__init__.py
@@ -156,7 +156,7 @@ class CrossSeed(_PluginBase):
     # 插件图标
     plugin_icon = "qingwa.png"
     # 插件版本
-    plugin_version = "1.6.1"
+    plugin_version = "1.6.2"
     # 插件作者
     plugin_author = "233@qingwa"
     # 作者主页
