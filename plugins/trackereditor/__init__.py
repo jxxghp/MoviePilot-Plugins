dbe479c2fe08c633424c46c677b6d713e75fd183
jeblove <249972068@qq.com>
2024-03-28 21:44:21 +0800

    fix 【tracker替换】版本号

--- plugins/trackereditor/__init__.py
+++ plugins/trackereditor/__init__.py
@@ -19,7 +19,7 @@ class TrackerEditor(_PluginBase):
     # 插件图标
     plugin_icon = "trackereditor_A.png"
     # 插件版本
-    plugin_version = "1.4"
+    plugin_version = "1.5"
     # 插件作者
     plugin_author = "honue"
     # 作者主页
