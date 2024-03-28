44cb0a0d24f752d995d89170144d7990bbb15a3b
Allen <hotlcc@qq.com>
2024-03-28 06:11:59 +0000

    v1.1

--- plugins/downloaderhelper/__init__.py
+++ plugins/downloaderhelper/__init__.py
@@ -28,7 +28,7 @@ class DownloaderHelper(_PluginBase):
     # 插件图标
     plugin_icon = "DownloaderHelper.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "hotlcc"
     # 作者主页
