a138e2965832b75de062a279d66405ddf6208acc
Allen <hotlcc@qq.com>
2024-03-29 14:44:43 +0000

    下载器助手版本升级v1.2

--- plugins/downloaderhelper/__init__.py
+++ plugins/downloaderhelper/__init__.py
@@ -32,7 +32,7 @@ class DownloaderHelper(_PluginBase):
     # 插件图标
     plugin_icon = "DownloaderHelper.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "hotlcc"
     # 作者主页
