875cda1fc0180f048808755e545db342f3f8408c
luojianquan443 <2569646547@qq.com>
2024-04-01 21:48:55 +0800

    update

--- plugins/vcbanimemonitor/__init__.py
+++ plugins/vcbanimemonitor/__init__.py
@@ -77,7 +77,7 @@ class VCBAnimeMonitor(_PluginBase):
     # 插件图标
     plugin_icon = "vcbmonitor.png"
     # 插件版本
-    plugin_version = "1.6.6"
+    plugin_version = "1.7"
     # 插件作者
     plugin_author = "pixel@qingwa"
     # 作者主页
