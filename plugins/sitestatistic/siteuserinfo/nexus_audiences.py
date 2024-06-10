5dd961dea086f27829dbb41b7692043db6ea41d1
olly <dark.olly@gmail.com>
2024-06-10 13:48:35 +0800

    fix ade site statis

--- plugins/sitestatistic/siteuserinfo/nexus_audiences.py
+++ plugins/sitestatistic/siteuserinfo/nexus_audiences.py
@@ -13,6 +13,10 @@ class NexusAudiencesSiteUserInfo(NexusPhpSiteUserInfo):
     def match(cls, html_text: str) -> bool:
         return 'audiences.me' in html_text
 
+    def _parse_site_page(self, html_text: str):
+        super()._parse_site_page(html_text)
+        self._torrent_seeding_page = f"usertorrentlist.php?userid={self.userid}&type=seeding"
+
     def _parse_seeding_pages(self):
         self._torrent_seeding_headers = {"Referer": urljoin(self._base_url, self._user_detail_page)}
         super()._parse_seeding_pages()
