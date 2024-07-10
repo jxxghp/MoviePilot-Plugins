61dc98ff7c29fe4b8ec7997bb975ea5d387e68da
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-07-10 20:53:39 +0800

    feat: BrushFlow v3.4

--- plugins/brushflow/__init__.py
+++ plugins/brushflow/__init__.py
@@ -258,7 +258,7 @@ class BrushFlow(_PluginBase):
     # 插件图标
     plugin_icon = "brush.jpg"
     # 插件版本
-    plugin_version = "3.3"
+    plugin_version = "3.4"
     # 插件作者
     plugin_author = "jxxghp,InfinityPacer"
     # 作者主页
