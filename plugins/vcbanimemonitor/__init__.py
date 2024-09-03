215049de11780152ddf8dd5985b5b9cf115ebd40
Pixel-LH <2569646547@qq.com>
2024-09-03 12:58:17 +0800

    fix:vcbanimemonitor 修复处理异常

--- plugins/vcbanimemonitor/__init__.py
+++ plugins/vcbanimemonitor/__init__.py
@@ -76,7 +76,7 @@ class VCBAnimeMonitor(_PluginBase):
     # 插件图标
     plugin_icon = "vcbmonitor.png"
     # 插件版本
-    plugin_version = "1.8.2.1"
+    plugin_version = "1.8.2.2"
     # 插件作者
     plugin_author = "pixel@qingwa"
     # 作者主页
