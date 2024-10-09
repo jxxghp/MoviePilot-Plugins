aee0a654752806c33008a02c1fa39d5eadcb8cfd
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-10-10 02:11:58 +0800

    feat(MediaServerRefresh): v1.3

--- plugins.v2/mediaserverrefresh/__init__.py
+++ plugins.v2/mediaserverrefresh/__init__.py
@@ -19,7 +19,7 @@ class MediaServerRefresh(_PluginBase):
     # 插件图标
     plugin_icon = "refresh2.png"
     # 插件版本
-    plugin_version = "1.2"
+    plugin_version = "1.3"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
