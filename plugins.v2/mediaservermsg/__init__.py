c53d1e666276a6f8fc2b6455335aaf807662ac42
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-10-10 22:18:08 +0800

    feat(MediaServerMsg): v1.4

--- plugins.v2/mediaservermsg/__init__.py
+++ plugins.v2/mediaservermsg/__init__.py
@@ -18,7 +18,7 @@ class MediaServerMsg(_PluginBase):
     # 插件图标
     plugin_icon = "mediaplay.png"
     # 插件版本
-    plugin_version = "1.3"
+    plugin_version = "1.4"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
