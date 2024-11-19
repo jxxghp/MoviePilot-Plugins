b2d5e84058bec5c3cdd6280f0a3a94caf838e6bb
LongShengWen <long.shengwen@gxxmt.com>
2024-11-19 18:29:33 +0800

    短剧分类调试

--- plugins.v2/playletcategory_v2/__init__.py
+++ plugins.v2/playletcategory_v2/__init__.py
@@ -25,11 +25,11 @@ class PlayletCategory_v2(_PluginBase):
     # 插件图标
     plugin_icon = "Amule_A.png"
     # 插件版本
-    plugin_version = "2.1"
+    plugin_version = "2.2"
     # 插件作者
-    plugin_author = "jxxghp"
+    plugin_author = "longqiuyu"
     # 作者主页
-    author_url = "https://github.com/jxxghp"
+    author_url = "https://github.com/LongShengWen"
     # 插件配置项ID前缀
     plugin_config_prefix = "playletcategory_v2_"
     # 加载顺序
