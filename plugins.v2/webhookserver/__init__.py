a8ebe051586aba947f49e22acb13c80b76d27f87
KoWming <1075151242@qq.com>
2024-12-11 17:22:29 +0800

    up

--- plugins.v2/webhookserver/__init__.py
+++ plugins.v2/webhookserver/__init__.py
@@ -16,7 +16,7 @@ class WebhookServer(_PluginBase):
     # 插件图标
     plugin_icon = "forward.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "jxxghp,KoWming"
     # 作者主页
