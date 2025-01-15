001ea26cdadf7f8ede2a1514846ec2d26a33e9e2
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2025-01-15 16:29:08 +0800

    fix(BrushFlow): v4.3.1

--- plugins.v2/brushflow/__init__.py
+++ plugins.v2/brushflow/__init__.py
@@ -251,7 +251,7 @@ class BrushFlow(_PluginBase):
     # 插件图标
     plugin_icon = "brush.jpg"
     # 插件版本
-    plugin_version = "4.3"
+    plugin_version = "4.3.1"
     # 插件作者
     plugin_author = "jxxghp,InfinityPacer"
     # 作者主页
