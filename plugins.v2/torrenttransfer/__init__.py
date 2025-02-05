e02470e70d421ec0e305215ea5c98bffde17bd47
so1ve <i@mk1.io>
2025-02-05 13:14:15 +0800

    chore: update version

--- plugins.v2/torrenttransfer/__init__.py
+++ plugins.v2/torrenttransfer/__init__.py
@@ -28,7 +28,7 @@ class TorrentTransfer(_PluginBase):
     # 插件图标
     plugin_icon = "seed.png"
     # 插件版本
-    plugin_version = "1.10"
+    plugin_version = "1.10.1"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
