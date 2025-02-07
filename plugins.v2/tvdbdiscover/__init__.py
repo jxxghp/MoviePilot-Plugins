ddd56509e9f435619156bfb57877ec9c30818e1f
jxxghp <jxxghp@gmail.com>
2025-02-07 19:55:41 +0800

    更新 __init__.py

--- plugins.v2/tvdbdiscover/__init__.py
+++ plugins.v2/tvdbdiscover/__init__.py
@@ -20,7 +20,7 @@ class TvdbDiscover(_PluginBase):
     # 插件图标
     plugin_icon = "TheTVDB_A.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
