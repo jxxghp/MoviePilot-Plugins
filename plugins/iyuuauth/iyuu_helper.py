bbfe3af9b3db4841e39602324a8186a7cb837b86
jxxghp <jxxghp@gmail.com>
2025-03-02 14:18:07 +0800

    更新 iyuu_helper.py

--- plugins/iyuuauth/iyuu_helper.py
+++ plugins/iyuuauth/iyuu_helper.py
@@ -6,7 +6,7 @@ from app.utils.http import RequestUtils
 
 class IyuuHelper(object):
     _version = "2.0.0"
-    _api_base = "http://api.bolahg.cn/%s"
+    _api_base = "http://api.iyuu.cn/%s"
     _sites = {}
     _token = None
 
