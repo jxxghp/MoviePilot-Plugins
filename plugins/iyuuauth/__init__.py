b48135973ec6131f38727cf22ab3599b55488254
jxxghp <jxxghp@gmail.com>
2025-03-02 14:18:22 +0800

    更新 __init__.py

--- plugins/iyuuauth/__init__.py
+++ plugins/iyuuauth/__init__.py
@@ -14,7 +14,7 @@ class IyuuAuth(_PluginBase):
     # 插件图标
     plugin_icon = "Iyuu_A.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
