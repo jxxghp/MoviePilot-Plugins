462954a21d6d9585a70b3ab82eafe8b9b97772bc
TimoYoung <yuxuanyang1@163.com>
2025-04-21 17:44:28 +0800

    v1.1

--- plugins/autosubv2/__init__.py
+++ plugins/autosubv2/__init__.py
@@ -38,7 +38,7 @@ class AutoSubv2(_PluginBase):
     # 主题色
     plugin_color = "#2C4F7E"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "TimoYoung"
     # 作者主页
