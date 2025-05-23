325996dfb13e547927b12142eecf741e1c54a45b
jxxghp <jxxghp@gmail.com>
2025-05-23 16:35:07 +0800

    更新 __init__.py

--- plugins.v2/brushflow/__init__.py
+++ plugins.v2/brushflow/__init__.py
@@ -259,7 +259,7 @@ class BrushFlow(_PluginBase):
     # 插件图标
     plugin_icon = "brush.jpg"
     # 插件版本
-    plugin_version = "4.3.2"
+    plugin_version = "4.3.3"
     # 插件作者
     plugin_author = "jxxghp,InfinityPacer"
     # 作者主页
