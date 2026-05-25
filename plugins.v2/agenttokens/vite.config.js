944867f96e8f3f62da0ab17b62fe489518db8b27
jxxghp <jxxghp@gmail.com>
2026-05-25 15:36:15 +0800

    fix: 设置base为相对路径

--- plugins.v2/agenttokens/vite.config.js
+++ plugins.v2/agenttokens/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import federation from '@originjs/vite-plugin-federation'
 
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     federation({
