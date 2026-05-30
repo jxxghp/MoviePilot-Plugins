c2dbaf5c5eb0c6df37a7616c6f534d7003f7a5a1
jxxghp <jxxghp@gmail.com>
2026-05-30 22:07:14 +0800

    chore(ChatGPT): v3.0.4

--- plugins.v2/chatgpt/__init__.py
+++ plugins.v2/chatgpt/__init__.py
@@ -47,7 +47,7 @@ class ChatGPT(_PluginBase):
     # 插件图标
     plugin_icon = "Chatgpt_A.png"
     # 插件版本
-    plugin_version = "3.0.3"
+    plugin_version = "3.0.4"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
