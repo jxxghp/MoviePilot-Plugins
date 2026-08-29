1fe189103195b8d0792d4dde35c1c4d92ba61d46
cosini <1966944566@qq.com>
2026-08-30 03:30:56 +0800

    Update IqiyiDiscover V2 to v1.1.0

--- plugins.v2/iqiyidiscover/__init__.py
+++ plugins.v2/iqiyidiscover/__init__.py
@@ -164,7 +164,7 @@ class IqiyiDiscover(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/main/icons/iqiyi_A.png"
     # 插件版本
-    plugin_version = "1.1.1"
+    plugin_version = "1.1.0"
     # 插件作者
     plugin_author = "LLL001a"
     # 作者主页
